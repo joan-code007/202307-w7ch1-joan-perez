18877862334853c4296eee70b1a4ad81b16f4507
Joan Pérez <joanperez.lapuente@gmail.com>
2023-08-21 21:46:25 +0200

    Create types and thingsData

---
+++ src/data/thingsData.ts
@@ -0,0 +1,20 @@
+import { type Thing } from "./types";
+
+export const thingsData: Thing[] = [
+  {
+    id: 1,
+    name: "Play football",
+  },
+  {
+    id: 2,
+    name: "Play guitar",
+  },
+  {
+    id: 3,
+    name: "Play padel",
+  },
+  {
+    id: 4,
+    name: "Travel",
+  },
+];
