82c0dad606e3e06aacdc2be92b7b753e65f9226c
Joan Pérez <joanperez.lapuente@gmail.com>
2023-08-22 02:01:56 +0200

    Modify files to .js

--- src/server/controllers/thingsControllers.ts
+++ src/server/controllers/thingsControllers.ts
@@ -1,5 +1,5 @@
 import { type Request, type Response } from "express";
-import { things } from "../../data/thingsData";
+import { things } from "../../data/thingsData.js";
 
 export const getThings = (_req: Request, res: Response) => {
   console.log("Request geThings to get all things");
--- src/server/routers/thingsRouter.ts
+++ src/server/routers/thingsRouter.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { getThings } from "../controllers/thingsControllers";
+import { getThings } from "../controllers/thingsControllers.js";
 
 const thingsRouter = express.Router();
 
