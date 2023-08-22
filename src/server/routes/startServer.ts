b53e87793153cfbc82fe7b33542beebda0b7a4ae
Joan Pérez <joanperez.lapuente@gmail.com>
2023-08-22 20:40:49 +0200

    Add test getThings status

--- src/server/routes/startServer.ts
+++ src/server/routes/startServer.ts
@@ -2,7 +2,7 @@ import app from "./index.js";
 
 const startServer = (port: string | number) => {
   app.listen(Number(port), () => {
-    console.log(`Listening on http://localhost:${port}/things `);
+    console.log(`Listening on http://localhost:${port}/`);
   });
 };
 
